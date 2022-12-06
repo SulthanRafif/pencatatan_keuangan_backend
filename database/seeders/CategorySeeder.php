213334e9cefc38f579de49425407fe97d69f25f3
SulthanRafif <rsulthan5@gmail.com>
2022-12-06 21:46:43 +0700

    Create seeders

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Category::create([
+            'name' => 'Food',
+        ]);
+
+        Category::create([
+            'name' => 'Clothing',
+        ]);
+
+        Category::create([
+            'name' => 'Device',
+        ]);
+
+        Category::create([
+            'name' => 'Makeup',
+        ]);
+
+        Category::create([
+            'name' => 'Holiday',
+        ]);
+    }
+}
