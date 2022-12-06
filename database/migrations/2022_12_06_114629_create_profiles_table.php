23e059fd7684340868dd553ec683e2ce6f049ab1
SulthanRafif <rsulthan5@gmail.com>
2022-12-06 19:08:11 +0700

    add controller + migration profile

---
+++ database/migrations/2022_12_06_114629_create_profiles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('fullname');
+            $table->string('address');
+            $table->string('place_of_birth');
+            $table->string('date_of_birth');
+            $table->string('profession');
+            $table->string('gender');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
