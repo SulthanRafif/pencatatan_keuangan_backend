c7867b24f94ccac0fc5c6dd96b818022b0f3840c
SulthanRafif <rsulthan5@gmail.com>
2022-12-07 09:04:57 +0700

    add route for auth and category

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,28 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PassportAuthController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+/**
+ * v1 API
+ */
+Route::group(['prefix' => 'v1'], function () {
+    // Auth
+    Route::post('register', [PassportAuthController::class, 'register']);
+    Route::post('login', [PassportAuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    // Category
+    Route::get('category', [CategoryController::class, 'index'])->name('api.category.index');
+    Route::get('category/{category}', [CategoryController::class, 'show']);
+
+    Route::middleware('auth:api')->group(function () {
+        // Auth
+        Route::post('logout', [PassportAuthController::class, 'logout']);
+
+        // Category
+        Route::post('category', [CategoryController::class, 'store'])->name('api.category.store');
+        Route::put('category/{category}', [CategoryController::class, 'update'])->name('api.category.update');
+        Route::delete('category/{category}', [CategoryController::class, 'destroy']);
+    });
 });
